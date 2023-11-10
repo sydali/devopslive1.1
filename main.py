13a3addbda9c6f05cd56238a79305d0c904da5a9
sydali <sydali@gmail.com>
2023-11-10 12:07:05 +1100

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,8 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    #return {"Hello": "World"}
-	return JsonResponse
-
+    return {"Hello": "World"}
+	
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
